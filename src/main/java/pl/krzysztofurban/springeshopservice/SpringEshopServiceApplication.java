16ca31671756c491d232916e2f393e9f9a554ab0
Krzysztof Urban <k.urban@g.pl>
2020-03-22 15:30:37 +0100

    Add properties

---
+++ src/main/java/pl/krzysztofurban/springeshopservice/SpringEshopServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.krzysztofurban.springeshopservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEshopServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEshopServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/krzysztofurban/springeshopservice/SpringEshopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krzysztofurban.springeshopservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEshopServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
